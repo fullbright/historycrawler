084d3160ebe7cadc40a402e2552d15f2d42de4d9
Full Bright <ful3right@gmail.com>
2016-01-19 20:31:20 +0100

    Add a script to wrap the installation process up

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo *************************************
+echo **                           	    **
+echo ** 	HISTORY CRAWLER		        **
+echo ** 	Full Installation	        **
+echo ** 				                **
+echo *************************************
+
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+
+echo "Installing base tools"
+
+
+echo "Installing necessary tools for history crawler to work"
+
+
+echo "Setting few things up"
+
+
+echo "Great ! Now we can run the application"
+
+
