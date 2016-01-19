b1adb0a2b77e5d423b6e19ebe22ef4f85cbd7568
Full Bright <ful3right@gmail.com>
2016-01-19 20:35:50 +0100

    Adding applications to start

--- run.sh
+++ run.sh
@@ -16,6 +16,19 @@ fi
 
 echo "Starting history crawler"
 
+echo "Starting Google Refine ..."
+./google-refine-2.5/refine &
+echo "Done."
 
+echo "Starting overview ..."
+cd overview*
+./run &
+cd ..
+echo "Done"
 
+echo "Starting Meandre ..."
+sh ./Meandre-1.4.12/Start-Infrastructure.sh
+sh ./Meandre-1.4.12/Start-Workbench.sh
+echo "Done."
 
+echo "Finished starting all the applications."
