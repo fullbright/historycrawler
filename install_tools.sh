cf3adac0579a996c4aa3142ab469d5344fc3af16
Full Bright <ful3right@gmail.com>
2016-01-19 20:30:38 +0100

    change the owner of the vim_runtime folder to pi user

--- install_tools.sh
+++ install_tools.sh
@@ -19,7 +19,7 @@ apt-get update --yes
 apt-get upgrade --yes
 
 echo "Install git first"
-apt-get install git -y
+apt-get install git curl -y
 
 echo "The install vim and the awesome vimrc"
 # awesome vimrc requires ctags
@@ -29,5 +29,7 @@ apt-get install ctags -y
 git clone https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
+echo "Give .vim_runtime folder ownership to pi user"
+chown pi:pi ~/.vim_runtime -R
 
 echo ** Installation finished	**
