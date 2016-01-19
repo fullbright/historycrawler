22cd0151c7566eaa9bc7df045f6bcc795fe56798
Full Bright <ful3right@gmail.com>
2016-01-19 20:31:04 +0100

    Adding a script to install and start the application. TODO: Use a file to flag that the installation was successfull

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo *************************************
+echo **                           	    **
+echo ** 	HISTORY CRAWLER		        **
+echo ** 	Run applications	        **
+echo ** 				                **
+echo *************************************
+
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+
+
+echo "Starting history crawler"
+
+
+
+
