c578a20b2ae010a31ce3bef02a87ce6d33bda728
Full Bright <full3right@gmail.com>
2016-04-09 15:36:10 +0200

    Adding customizations to vim awesome

--- install_base_tools.sh
+++ install_base_tools.sh
@@ -34,6 +34,10 @@ apt-get install aptitude -y
 git clone https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
+echo "Adding customiations to vim awesome"
+echo "colorscheme mayansmoke" >> ~/.vim_runtime/my_configs.vim
+echo "colorscheme peaksea" >> ~/.vim_runtime/my_configs.vim
+
 echo "Give .vim_runtime folder ownership to $CURRENT_USER user"
 chown $CURRENT_USER:$CURRENT_USER ~/.vim_runtime -R
 
